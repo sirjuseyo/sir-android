1ba0d765e3efb0a353ff6142048d9f7987cff9f0
ian <developer.kss@gmail.com>
2019-08-19 14:02:06 +0900

    remove dead code

--- sir_lib/src/main/java/com/sirjuseyo/lib/type/JsonElementAsObject.kt
+++
@@ -1,3 +0,0 @@
-package com.sirjuseyo.lib.type
-
-class JsonElementAsObject(obj: Any) : JsonElementAs(obj, OutputType.ToObject)
--- sir_lib/src/main/java/com/sirjuseyo/lib/type/JsonElementAsString.kt
+++
@@ -1,3 +0,0 @@
-package com.sirjuseyo.lib.type
-
-class JsonElementAsString(obj: Any) : JsonElementAs(obj, OutputType.ToString)
